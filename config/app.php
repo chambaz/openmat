4c83aecdebbf8559b55cac7f36b045fdccc090fb
Adam Chambers <adammchambers@gmail.com>
2019-10-13 10:28:40 -0400

    🔧 Configure geocoder package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\Geocoder\GeocoderServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Geocoder' => Spatie\Geocoder\Facades\Geocoder::class,
+        
     ],
 
 ];
