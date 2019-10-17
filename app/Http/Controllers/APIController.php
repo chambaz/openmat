8209e04bc37dbc6c0db8347d302b4c51bf64f05b
Adam Chambers <adammchambers@gmail.com>
2019-10-17 00:29:48 -0400

    🚧 Remove user join from events API endpoint and validation for required lat / lng params

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -9,17 +9,22 @@ class APIController extends Controller
 {
     public function events(Request $request)
     {
+        if (!$request->has(['lat', 'lng'])) {
+            return response()->json(array(
+                'error' => 'Lat / lng parameters required'
+            ))->setStatusCode(400);
+        }
+
         $lat = $request->input('lat');
         $lng = $request->input('lng');
         $miles = $request->input('radius', 100);
 
         $events = DB::select("
-            SELECT events.*, users.* FROM events, users WHERE
+            SELECT * FROM events WHERE
             events.latitude BETWEEN ({$lat} - ({$miles}*0.018)) AND ({$lat} + ({$miles}*0.018)) AND
-            events.longitude BETWEEN ({$lng} - ({$miles}*0.018)) AND ({$lng} + ({$miles}*0.018)) AND
-            events.user_id = users.id;
+            events.longitude BETWEEN ({$lng} - ({$miles}*0.018)) AND ({$lng} + ({$miles}*0.018));
         ");
-        
+
         return response()->json($events);
     }
 }
