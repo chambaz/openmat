e53f3be23b1e740f359e049e566be20de378fd1f
Adam Chambers <adammchambers@gmail.com>
2019-10-17 02:03:48 -0400

    🚧 Static listing filter front end

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -2,6 +2,32 @@
 
 @section('content')
 <div class="container">
+    <form>
+        <div class="form-row" style="padding: 10px 0 40px;">
+            <div class="col-md-3">
+                <label style="margin-top: 10px;" for="inputEmail4">Address</label>
+                <input type="text" class="form-control" placeholder="e.g 1175 State Street">
+            </div>
+            <div class="col-md-3">
+                <label style="margin-top: 10px;" for="inputEmail4">Date</label>
+                <input type="date" class="form-control" placeholder="Date">
+            </div>
+            <div class="col-md-3">
+                <label style="margin-top: 10px;" for="inputEmail4">Search...</label>
+                <input type="search" class="form-control" placeholder="e.g Ronin BJJ">
+            </div>
+            <div class="col-md-3">
+                <label style="margin-top: 10px;" for="inputEmail4">Radius</label>
+                <select id="inputState" class="form-control">
+                    <option selected>50</option>
+                    <option>100</option>
+                    <option>200</option>
+                    <option>300</option>
+                    <option>400</option>
+                </select>
+            </div>
+        </div>
+    </form>
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach ($events as $event)
